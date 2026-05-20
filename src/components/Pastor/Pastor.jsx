f720b89a221fe9268de82004c1b2b83c82452795
user <user@MacBook-Kalombo.local>
2026-05-20 08:58:22 +0100

    Ajout de la biographie du Pasteur

--- src/components/Pastor/Pastor.jsx
+++ src/components/Pastor/Pastor.jsx
@@ -23,11 +23,14 @@ const Pastor = () => {
               Le pasteur Yves Kalombo est né le 14 juillet 1959 à Dekese, dans l’ex-Kasaï Occidental, au sein d’une famille de douze enfants dont il est le deuxième. Son père, Kalombo Kabwe Simon Pierre, a travaillé dans l’administration publique avant de terminer sa carrière comme cadre à la SNCC, tandis que sa mère s’appelait Victorine Tshibwabwa Tshimanga.
             </p>
             <p className="pastor-text">
-              Son ministère est marqué par la puissance de la prière, le miracle de la
-              transformation intérieure et un engagement indéfectible envers le réveil spirituel
-              des nations. Rejoignez-nous pour expérimenter cette atmosphère de gloire.
+              En raison des mutations professionnelles de son père, son parcours scolaire l’a conduit dans plusieurs villes du pays. Il a fait ses études primaires au Collège Saint-Louis de Kananga, puis a poursuivi sa scolarité à Kinshasa, notamment à l’Athénée de la Gombe. Ses études secondaires se sont déroulées à Lubumbashi, au Collège Imara et à l’Institut Kiwele.
             </p>
-
+            <p className="pastor-text">
+              Plus tard, à Kinshasa, il a obtenu le diplôme d’ingénieur technicien en électronique radionavigation à l’Institut Supérieur des Techniciens Appliqués (ISTA). Par la suite, il s’est également formé en journalisme et en informatique au centre CEFOJI. Son profil reflète donc une double orientation, à la fois technique et communicative, qu’il mettra plus tard au service de l’évangélisation et de l’enseignement biblique.
+            </p>
+            <p className="pastor-text">
+              En 1984, il s’est marié à Mademoiselle Gorettie Twebana Kalombo, et le couple est parent de huit enfants. En 2003, il a également créé un magazine d’informations religieuses basé à Kinshasa. Cette dimension médiatique montre son intérêt pour la diffusion du message chrétien au-delà de la chaire et de l’assemblée.</p>
+            <p className="pastor-text">Son parcours spirituel est marqué par une recherche profonde du sens de la vie dès sa jeunesse. Pendant ses années d’études, il a connu une expérience de conversion qui a transformé sa vision de l’existence et orienté toute sa vie vers la prédication de l’Évangile. Selon son témoignage, cette rencontre avec Dieu a fait naître en lui une conviction durable : servir Jésus-Christ et annoncer Sa Parole avec fidélité. </p>
             <div className="pastor-signature">
               <span className="signature-name">Rev. KALOMBO Yves</span>
               <span className="signature-title">Pasteur Principal</span>
